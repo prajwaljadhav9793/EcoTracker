ed81a824d4eac79f47ad6ee1df338011e25290dc
Prajwal Jadhav <prajwaljadhav9793@gmail.com>
2026-05-08 19:54:58 +0530

    space

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
+   
 from flask import Flask, render_template, request, redirect, session
 
 from modules.user import (
